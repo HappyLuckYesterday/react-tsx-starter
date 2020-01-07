1af404ef432321a703545b5fd1f3e224816550c8
happylucktoday <happyluck202401@outlook.com>
2020-01-07 23:04:04 +0100

    init

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { render } from 'react-dom';
+
+const App = () => {
+	return (
+		<div className="container py-2">
+			test
+		</div>
+	)
+}
+
+render(<App />, document.getElementById('root'));
