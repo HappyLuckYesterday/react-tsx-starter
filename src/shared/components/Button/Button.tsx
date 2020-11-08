a6b95bc7382f9d4d484b45c37dc1490c24315fc6
happylucktoday <happyluck202401@outlook.com>
2020-11-08 18:19:25 +0100

    added component enum added cssClasses to button added props to button

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { COLOR, VARIANT } from '../component.enums';
 
 interface IButtonProps {
+	block?: boolean;
 	children: React.ReactNode;
+	color?: COLOR;
+	disabled?: boolean;
+	isRounded?: boolean;
+	isActive?: boolean;
 	onClick?: (e: React.MouseEvent) => void;
+	variant?: VARIANT;
 }
 
-export const Button = ({ children, onClick }: IButtonProps) => {
+export const Button = ({
+	children,
+	block,
+	color = COLOR.primary,
+	disabled,
+	isRounded,
+	isActive,
+	onClick, variant
+}: IButtonProps) => {
+
+	useEffect(() => {
+		console.warn('init');
+	}, []);
 
 	const getCssClasses = () => {
-		return '';
+		const cssClasses: Array<string> = [];
+		cssClasses.push(`btn`);
+		if (variant !== 'outline' && variant !== 'text') {
+			cssClasses.push(`btn-${color}`);
+		}
+		if (variant === 'outline') {
+			cssClasses.push(`btn-outline-${color}`);
+		}
+		if (variant === 'text') {
+			cssClasses.push(`btn-link`);
+			cssClasses.push(`btn-link-${color}`);
+		}
+		if (isRounded && variant !== 'text') {
+			cssClasses.push(`rounded-pill`);
+		}
+		if (block) {
+			cssClasses.push('btn-block');
+		}
+		if (isActive) {
+			cssClasses.push('active');
+		}
+		return cssClasses.join(' ');
 	};
 
-	const disabled = false;
-
 	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-		onClick(e);
+		onClick ? onClick(e) : undefined;
 	};
 
 	return (
---
+++ src/shared/components/component.enums.ts
@@ -0,0 +1,14 @@
+export enum COLOR {
+	primary = 'primary',
+	accent = 'accent',
+	secondary = 'secondary',
+	danger = 'danger',
+	light = 'light',
+	dark = 'dark'
+}
+
+export enum VARIANT { normal = 'normal', outline = 'outline', text = 'text' }
+
+export enum SIZE { sm = 'sm', md = 'md', lg = 'lg' }
+
+export enum POSITION { right = 'right', left = 'left' }
