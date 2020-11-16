cf75666f45fdff21dcd875decf99467156b72a7b
happylucktoday <happyluck202401@outlook.com>
2020-11-16 13:04:36 +0100

    fixed return typing

--- src/shared/services/http.service.ts
+++ src/shared/services/http.service.ts
@@ -1,20 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 // const CLASSNAME = 'HttpService';
 export class HttpService {
-	get<T>(url: string, params?: any): Promise<T> {
+	get<T>(url: string, params?: any): Promise<AxiosResponse<T>> {
 		return axios.get(url, { params: params });
 	}
 
-	post<T>(url: string, data: any): Promise<T> {
+	post<T>(url: string, data: any): Promise<AxiosResponse<T>> {
 		return axios.post(url, data);
 	}
 
-	put<T>(url: string, body: any): Promise<T> {
+	put<T>(url: string, body: any): Promise<AxiosResponse<T>> {
 		return axios.put(url);
 	}
 
-	delete<T>(url: string, params?: any): Promise<T> {
+	delete<T>(url: string, params?: any): Promise<AxiosResponse<T>> {
 		return axios.delete(url);
 	}
 
